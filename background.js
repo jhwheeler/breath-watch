5d8cc62a3afb150a16ddcc52000ffdde024a1c78
Jackson Holiday Wheeler <jhwheeler@gmail.com>
2019-12-07 17:33:19 +0700

    feat: notifications

--- background.js
+++ background.js
@@ -2,6 +2,15 @@ function triggerReminder () {
   chrome.storage.sync.set({ openedReminder: Date.now() }, () => {
     console.log('watch your breath now: ', Date.now())
   })
+
+  const notificationOptions = {
+    type: "basic",
+    title: "Watch your breath",
+    message: "It's time to become mindful of your breath.",
+    iconUrl: "images/get_started128.png"
+  }
+
+  chrome.notifications.create(`breath_watch_reminder_${Date.now()}`, notificationOptions)
 }
 
 chrome.runtime.onInstalled.addListener(() => triggerReminder())
