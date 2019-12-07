731c2c64199fb173b56b79f7dfd8992895e551df
Jackson Holiday Wheeler <jhwheeler@gmail.com>
2019-12-07 23:16:18 +0700

    feat: basic options page

---
+++ options.js
@@ -0,0 +1,33 @@
+function saveOptions () {
+  var interval = document.getElementById('interval').value
+  var duration = document.getElementById('duration').value
+
+  chrome.storage.sync.set({
+    awarenessReminderInterval: interval,
+    awarenessReminderDuration: duration,
+  }, () => {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status')
+    status.textContent = 'Options saved'
+
+    setTimeout(() => {
+      status.textContent = ''
+    }, 750)
+  });
+}
+
+function restoreOptions () {
+  const defaultInterval = '60'
+  const defaultDuration = '1'
+
+  chrome.storage.sync.get({
+    awarenessReminderInterval: defaultInterval,
+    awarenessReminderDuration: defaultDuration,
+  }, items => {
+    document.getElementById('interval').value = items.awarenessReminderInterval
+    document.getElementById('duration').value = items.awarenessReminderDuration;
+  })
+}
+
+document.addEventListener('DOMContentLoaded', restore_options)
+document.getElementById('save').addEventListener('click', save_options)
